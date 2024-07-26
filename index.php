8241efc516102d08db5e04b39db6cdc5e39019bd
Clodomilson <clodomilsonanjos.eng@outlook.com>
2024-07-26 15:57:19 -0300

    "Alteração CSS INDEX"

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Banco de Sangue - Seja um Doador</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/index-style.css">
+   <link rel="stylesheet" href="css/index-style.css">
     <link rel="icon" href="img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> <!-- Font Awesome -->
 </head>
