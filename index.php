7de064b59149003df8471dc18e54f98ffe092b4f
lucascunhareisSilva <155857539+lucascunhareisSilva@users.noreply.github.com>
2024-08-05 17:20:53 -0300

    update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Banco de Sangue</a>
+        <a class="navbar-brand" href="#">SANGUE SOLIDÁRIO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
